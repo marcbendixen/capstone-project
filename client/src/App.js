50adab1668af5fc081e3ce58c1939a61237d87a4
Marc Bendixen <moin@marcbendixen.de>
2021-06-06 23:40:17 +0200

    Add PopularList to app & add example data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import PosterList from './components/PosterList'
+import popularList from './example_data/popular.json'
+
 function App() {
-  return <h1>Serientracker</h1>
+  return (
+    <>
+      <h1>Serientracker</h1>
+      <PosterList list={popularList.results} />
+    </>
+  )
 }
 
 export default App
