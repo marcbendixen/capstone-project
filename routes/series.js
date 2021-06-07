fddbfd816fbb3fb4fdc197e5f7a8080433e834f5
Marc Bendixen <moin@marcbendixen.de>
2021-06-07 16:38:39 +0200

    Add basic routing

---
+++ routes/series.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+const { API_BASE_URL, API_KEY, API_LANGUAGE } = process.env
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const url = `${API_BASE_URL}/${id}?api_key=${API_KEY}&language=${API_LANGUAGE}`
+
+  await fetch(url)
+    .then(res => res.json())
+    .then(data => res.send(data))
+})
+
+router.get('/:id/credits', async (req, res) => {
+  const { id } = req.params
+  const url = `${API_BASE_URL}/${id}/credits?api_key=${API_KEY}&language=${API_LANGUAGE}`
+
+  await fetch(url)
+    .then(res => res.json())
+    .then(data => res.send(data))
+})
+
+router.get('/:id/season/:season', async (req, res) => {
+  const { id, season } = req.params
+  const url = `${API_BASE_URL}/${id}/season/${season}?api_key=${API_KEY}&language=${API_LANGUAGE}`
+
+  await fetch(url)
+    .then(res => res.json())
+    .then(data => res.send(data))
+})
+
+router.get('/popular', async (req, res) => {
+  const url = `${API_BASE_URL}/popular?api_key=${API_KEY}&language=${API_LANGUAGE}`
+
+  await fetch(url)
+    .then(res => res.json())
+    .then(data => res.send(data))
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 4000
 
-app.use('/api', (req, res) => res.send('Hello from the serientracker API!'))
+const { PORT = 4000 } = process.env
+
+app.use('/api', express.json())
+app.use('/api/series', require('./routes/series'))
 
 app.use((req, res) => res.sendStatus(404))
 
-app.listen(port, () => {
-  console.log(`Server startet at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server startet at http://localhost:${PORT}`)
 })
