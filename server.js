97058ae4e414f6df70163aa113e76cb810136c89
Marc Bendixen <moin@marcbendixen.de>
2021-06-07 23:59:50 +0200

    Update server routing

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
-require('dotenv').config()
+const path = require('path')
 const app = express()
+require('dotenv').config()
 
 const { PORT = 4000 } = process.env
 
 app.use('/api', express.json())
 app.use('/api/series', require('./routes/series'))
+app.use(express.static('client/build'))
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 app.use((req, res) => res.sendStatus(404))
 
