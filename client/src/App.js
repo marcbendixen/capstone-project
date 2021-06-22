21fcc4e24c487a1f03e2c0625fe6b9e88e27b4bc
Marc Bendixen <moin@marcbendixen.de>
2021-06-22 23:58:17 +0200

    Refactor App.js & hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,16 @@
-import { useEffect, useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import Header from './components/Header'
 import PosterList from './components/PosterList'
+import useEpisodes from './hooks/useEpisodes'
+import useSeries from './hooks/useSeries'
+import useWatchlist from './hooks/useWatchlist'
 import SeriesDetailsPage from './pages/SeriesDetailsPage'
-import getPopular from './services/getPopular'
-import getSeriesDetails from './services/getSeriesDetails'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 export default function App() {
-  const [series, setSeries] = useState([])
-  const [watchlist, setWatchlist] = useState([])
-  const [watchlistIds, setWatchlistIds] = useState(
-    getLocalStorage('watchlistIds') ?? []
-  )
-  const [watchedEpisodesIds, setWatchedEpisodesIds] = useState(
-    getLocalStorage('watchedEpisodesIds') ?? []
-  )
-
-  console.log('WATCHLIST: ', watchlist)
-
-  useEffect(() => {
-    series.length === 0 &&
-      getPopular()
-        .then(data => {
-          Promise.all(
-            data.results.map(({ id }) =>
-              getSeriesDetails(id).then(
-                data => (data = { ...data, isPopular: true })
-              )
-            )
-          ).then(data => {
-            setSeries(data)
-          })
-        })
-        .catch(error => {
-          console.error('Error:', error)
-        })
-  }, [series])
-
-  useEffect(() => {
-    setLocalStorage('watchedEpisodesIds', watchedEpisodesIds)
-  }, [watchedEpisodesIds])
-
-  useEffect(() => {
-    setLocalStorage('watchlistIds', watchlistIds)
-  }, [watchlistIds])
-
-  useEffect(() => {
-    Promise.all(
-      watchlistIds.map(id => getSeriesDetails(id).then(data => data))
-    ).then(data => setWatchlist(data.reverse()))
-  }, [watchlistIds])
+  const { series, handleNewSeries } = useSeries()
+  const { watchlist, handleWatchlist, checkIsOnWatchlist } = useWatchlist()
+  const { handleCheckEpisode, checkIsEpisodeWatched } = useEpisodes()
 
   return (
     <Container>
@@ -93,54 +52,13 @@ export default function App() {
       </StyledMain>
     </Container>
   )
-
-  function handleNewSeries(data) {
-    setSeries([...series, data])
-  }
-
-  function handleWatchlist(id) {
-    const seriesId = Number(id)
-    const isOnList = watchlistIds.some(el => el === seriesId)
-
-    if (isOnList) {
-      const indexToRemove = watchlistIds.findIndex(el => el === seriesId)
-      setWatchlistIds([
-        ...watchlistIds.slice(0, indexToRemove),
-        ...watchlistIds.slice(indexToRemove + 1),
-      ])
-    } else {
-      setWatchlistIds([...watchlistIds, seriesId])
-    }
-  }
-
-  function handleCheckEpisode(episodeId) {
-    const isOnList = watchedEpisodesIds.some(el => el === episodeId)
-
-    if (isOnList) {
-      const indexToRemove = watchedEpisodesIds.findIndex(el => el === episodeId)
-      setWatchedEpisodesIds([
-        ...watchedEpisodesIds.slice(0, indexToRemove),
-        ...watchedEpisodesIds.slice(indexToRemove + 1),
-      ])
-    } else {
-      setWatchedEpisodesIds([...watchedEpisodesIds, episodeId])
-    }
-  }
-
-  function checkIsEpisodeWatched(id) {
-    return watchedEpisodesIds.some(el => el === id)
-  }
-
-  function checkIsOnWatchlist(id) {
-    return watchlistIds.some(el => el === Number(id))
-  }
 }
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  max-width: 1024px;
+  max-width: 768px;
   width: 100%;
   margin-bottom: 32px;
 `
---
+++ client/src/hooks/useSeries.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import getPopular from '../services/getPopular'
+import getSeriesDetails from '../services/getSeriesDetails'
+
+export default function useSeries() {
+  const [series, setSeries] = useState([])
+
+  useEffect(() => {
+    series.length === 0 &&
+      getPopular()
+        .then(data => {
+          Promise.all(
+            data.results.map(({ id }) =>
+              getSeriesDetails(id).then(
+                data => (data = { ...data, isPopular: true })
+              )
+            )
+          ).then(data => {
+            setSeries(data)
+          })
+        })
+        .catch(error => {
+          console.error('Error:', error)
+        })
+  }, [series])
+
+  function handleNewSeries(data) {
+    setSeries([...series, data])
+  }
+
+  return { series, handleNewSeries }
+}
